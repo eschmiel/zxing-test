c99c94a9bf1d40a72a8f71bcbcf19517f264e0a1
Eric Schmiel <eric.schmiel@timmons.com>
2023-10-04 11:28:47 -0400

    zxing-test

---
+++ vite-test/vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import { BarcodeScanner } from './BarcodeScanner'
+
+function App() {
+  const [result, setResult] = useState('')
+  const [scanning, setScanningStatus] = useState(false)
+
+  const btnHandler = () => {
+    setScanningStatus(!scanning)
+  }
+
+  return (
+    <>
+      {scanning ? <BarcodeScanner setResult={setResult} /> : ''}
+      <button onClick={btnHandler}>toggle scanning</button>
+      <p>
+        <span>Last result:</span>
+        <span>{result}</span>
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ vite-test/vite-project/src/BarcodeScanner.jsx
@@ -0,0 +1,11 @@
+import { useZxing } from 'react-zxing';
+
+export const BarcodeScanner = ({ setResult }) => {
+    const { ref } = useZxing({
+        onDecodeResult(result) {
+            setResult(result.getText());
+        }
+    });
+
+    return (<video ref={ref} />)
+}
