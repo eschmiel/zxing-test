dda4b4ca6aa6a22cad28c14f42e835a6d43caf9f
Eric Schmiel <eric.schmiel@timmons.com>
2023-10-24 11:45:09 -0400

    device listing

--- vite-test/vite-project/src/App.jsx
+++ vite-test/vite-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
 import { BarcodeScanner } from './BarcodeScanner'
@@ -5,11 +6,26 @@ import { BarcodeScanner } from './BarcodeScanner'
 function App() {
   const [result, setResult] = useState('')
   const [scanning, setScanningStatus] = useState(false)
+  const [deviceCapabilities, setCapabilities] = useState([])
 
   const btnHandler = () => {
     setScanningStatus(!scanning)
   }
 
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      const capabilities = []
+      devices.forEach((device) => {
+        if (device.getCapabilities) {
+          const cap = device.getCapabilities()
+          capabilities.push(cap)
+        }
+        // const {aspectRatio, frameRate, height, width, resizeMode} = cap
+      })
+
+      setCapabilities(capabilities)
+    })
+  })
   return (
     <>
       {scanning ? <BarcodeScanner setResult={setResult} setScanningStatus={setScanningStatus} /> : ''}
@@ -18,6 +34,47 @@ function App() {
         <span>Last result:</span>
         <span>{result}</span>
       </p>
+      {deviceCapabilities.map((device, index) =>
+        <div key={index}>
+          <h1>device {index}</h1>
+          <h2>
+            aspectRatio:
+          </h2>
+          <p>
+            max: {device.aspectRatio?.max}
+          </p>
+          <p>
+            min: {device.aspectRatio?.min}
+          </p>
+          <h2>
+            frameRate:
+          </h2>
+          <p>
+            max: {device.frameRate?.max}
+          </p>
+          <p>
+            min: {device.frameRate?.min}
+          </p>
+          <h2>
+            height
+          </h2>
+          <p>
+            max: {device.height?.max}
+          </p>
+          <p>
+            min: {device.height?.min}
+          </p>
+          <h2>
+            width
+          </h2>
+          <p>
+            max: {device.width?.max}
+          </p>
+          <p>
+            min: {device.width?.min}
+          </p>
+        </div>
+      )}
     </>
   )
 }
